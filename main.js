593e3fc9d99cbd5726bc0f0f2b6bd6f4e9505567
TatianaIlieva <103999490+TatianaIlieva@users.noreply.github.com>
2022-06-26 16:04:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,58 @@
-﻿for (let i = 0; i < 100; i++) {
+for (let j = 0; j < 100; j++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    let parentBox = document.querySelector("#squares");
+    parentBox.appendChild(square);
+}
+
+function randomNumber() {
+    let numbers = [];
+    for (let k = 0; k < 100; k++) {
+        let n = Math.floor(Math.random() * 100);
+        let check = numbers.includes(n);
+        if (check === false) {
+            numbers.push(n);
+        } else {
+            while (check === true) {
+                n = Math.floor(Math.random() * 100);
+                check = numbers.includes(n);
+                if (check === false) {
+                    numbers.push(n);
+                }
+            }
+        }
+    }
+    return numbers;
+}
+
+let random = randomNumber();
+
+for (let i = 0; i < 100; i++) {
     setTimeout(function timer() {
-        const square = document.createElement("div");
-        square.className = "square";
-
-        var x = Math.floor(Math.random() * 256);
-        var y = Math.floor(Math.random() * 256);
-        var z = Math.floor(Math.random() * 256);
-        var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-
-        square.style.background = bgColor;
-
-        let parentBox = document.getElementById("squares");
-        parentBox.style.width = "600px";
-        parentBox.style.height = "600px";
-        parentBox.appendChild(square);
+        let squareColor = document.getElementById("squares").querySelectorAll(".square");
+        let arr = color();
+        let bgColor = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]}`;
+        squareColor[random[i]].style.background = bgColor;
 
         if (i == 99) {
-            let complete = document.createElement("div");
-            complete.className = "complete"
+            let complete = document.createElement("p");
+            complete.className = "complete";
             complete.innerHTML = "Complete";
             document.body.appendChild(complete);
         }
     }, i * 1000);
 }
 
+function color() {
+    let arrColor = [];
+    for (let j = 0; j < 3; j++) {
+        let x = Math.floor(Math.random() * 256).toString();
+        arrColor.push(x);
+    }
+    return arrColor;
+}
+
 document.getElementById("squares").addEventListener('click', function (e) {
-    var boxColor = window.getComputedStyle(e.target).background;
+    let boxColor = e.target.style.background;
     document.body.style.background = boxColor;
 });
